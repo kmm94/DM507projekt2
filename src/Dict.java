e151e1ada304706faf5b55c4d86f7e7540e28177
karim møller <Karimkuku@gmail.com>
2017-03-30 10:06:32 +0200

    formattede

--- src/Dict.java
+++ src/Dict.java
@@ -10,18 +10,21 @@
 public interface Dict {
     /**
      * Brugt til at indsætte en knude i træet.
+     *
      * @param key nøglen på knuden.
      */
     void insert(int key);
 
     /**
      * Brugt til at retunere hele træet som et sorteret array.
+     *
      * @return integer array
      */
     int[] orderedTraversal();
 
     /**
      * returnere true hvis key er i træet
+     *
      * @param key den int man vil søge efter.
      * @return
      */
