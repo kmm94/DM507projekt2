1c015be40b5c0d017c1efc181ad7e5498aea3921
karim møller <Karimkuku@gmail.com>
2017-03-30 09:52:15 +0200

    formattede og java doc

--- src/DictBinTree.java
+++ src/DictBinTree.java
@@ -20,7 +20,8 @@ public class DictBinTree implements Dict {
 
     /**
      * Inserts the nodes in the tree.
-     * @param key 
+     *
+     * @param key
      */
     @Override
     public void insert(int key) {
@@ -28,45 +29,45 @@ public class DictBinTree implements Dict {
         Node newNode = new Node(key);
         Node y = null;
         Node x = root;
-        while (x != null) {
+        while (x != null) { //is seraching for a place for the new node
             y = x;
             if (newNode.getKey() < x.getKey()) {
                 x = x.getLeftChild();
-
             } else {
                 x = x.getRightChild();
-
             }
         }
-        if (y == null) {
+        if (y == null) { // if root is empty
             root = newNode;
-        } else if (newNode.getKey() < y.getKey()) {
+        } else if (newNode.getKey() < y.getKey()) { //inserting the new note
             y.setLeftChild(newNode);
         } else {
             y.setRightChild(newNode);
         }
     }
-    
+
     /**
      * Traverses through the tree in inorder.
-     * @return 
+     *
+     * @return
      */
     @Override
     public int[] orderedTraversal() {
         int[] nodes = new int[size];
         counter = 0;
-        return  inorderTreeWalk(root, nodes);
+        return inorderTreeWalk(root, nodes);
     }
-    
+
     /**
      * Private method that traverses the tree inorder hereby sorting it.
+     *
      * @param x
      * @param a
-     * @return 
+     * @return
      */
     private int[] inorderTreeWalk(Node x, int[] a) {
 
-        if(x != null){
+        if (x != null) {
             inorderTreeWalk(x.getLeftChild(), a);
             a[counter] = x.getKey();
             counter++;
@@ -74,26 +75,28 @@ public class DictBinTree implements Dict {
         }
         return a;
     }
-    
+
     /**
      * Searches the tree for the nodes using the private method treeSearch().
+     *
      * @param key
-     * @return 
+     * @return
      */
     @Override
     public boolean search(int key) {
-    if (treeSearch(root, key) == null){
-        return false;
-    } else {
-        return true;
+        if (treeSearch(root, key) == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
-    }
-    
+
     /**
      * Private method that searches the tree for nodes.
+     *
      * @param x
      * @param key
-     * @return 
+     * @return
      */
     private Node treeSearch(Node x, int key) {
 
